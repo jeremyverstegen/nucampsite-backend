cacb4fecabf0c462824a5e2abe00a6d4fd7763f4
Jeremy Verstegen <jvmedia77@gmail.com>
2022-06-25 09:55:07 -0600

    Week 2 Assignment - Partners and Promotions

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,53 +1,76 @@
 const express = require('express');
+const Partner = require('../models/partner');
 const partnerRouter = express.Router();
 
+
 partnerRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res, next) => {
+    Partner.find()
+    .then(partners => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partners);
+    })
+    .catch(err => next(err));
 })
-
-.get((req, res) => {
-    res.end('Will send all the partners to you');
+.post((req, res, next) => {
+    Partner.create(req.body)
+    .then(partner => {
+        console.log('Partner Created ', partner)
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
 })
-
-.post((req, res) => {
-    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
-})
-
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not support on /partners');
 })
-
-.delete((req, res) => {
-    res.end('Deleting all partners');
+.delete((req, res, next) => {
+    Partner.deleteMany()
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
 
 partnerRouter.route('/:partnerId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
+.get((req, res, next) => {
+    Partner.findById(req.params.partnerId)
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
 })
-.get((req, res) => {
-    res.end('Will send the partner to you');
-})
-
 .post((req, res) => {
-    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
-})
-
-.put((req, res) => {
     res.statusCode = 403;
-    res.end('PUT operation not support on /partners/partnerId');
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
-
-.delete((req, res) => {
-    res.end(`Deleting the partner at parters/${req.params.partnerId}`);
+.put((req, res, next) => {
+    Partner.findByIdAndUpdate(req.params.partnerId, {
+        $set: req.body
+    }, { new: true })
+    .then(partner => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(partner);
+    })
+    .catch(err => next(err));
+})
+.delete((req, res, next) => {
+    Partner.findByIdAndDelete(req.params.partnerId)
+    .then(response => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(response);
+    })
+    .catch(err => next(err));
 });
-
 
 
 
